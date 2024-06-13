d117914778ebc192de5a7c1e68529771bb0d5ac7
mura123+ <mwsym1898@gmail.com>
2024-06-13 17:26:28 +0300

    add contact manager

--- ContactManagerTest/UnitTest1.cs
+++ ContactManagerTest/UnitTest1.cs
@@ -1,11 +1,89 @@
+using Xunit;
+using System.Runtime.CompilerServices;
+using ContactManagers;
+using System.Xml.Linq;
+
+
+
+
+
 namespace ContactManagerTest
 {
     public class UnitTest1
     {
+
         [Fact]
-        public void Test1()
+        public void AddContact()
         {
+            Program.Contacts.Clear();
+            //Arrange
+            string name1 = "Ahmed";
+            List<string> list = Program.Contacts;
+            //Act
+            List<string> result = Program.AddContact(name1);  // {Ahmed}
+
+            bool Contains = list.Contains("Ahmed");
+            //Assert
+            Assert.Equal(true, Contains);
+
+        }
+
+
+        [Fact]
+        public void duplicateAddContact()
+        {
+            Program.Contacts.Clear();
+
+            //Arragnge
+            string name1 = "mohammed";
+            string name2 = "Ali";
+            List<string> list = Program.Contacts;
+            //Act
+            List<string> result = Program.AddContact(name1);  // {mohammed}
+            result = Program.AddContact(name2);   // {mohammed,Ali}
+            result = Program.AddContact(name2);   // {mohammed,Ali}
+
+            int s = result.Count;
+            //Assert
+            Assert.Equal(2, s);
+        }
+
+        [Fact]
+        public void RemoveContact()
+        {
+            Program.Contacts.Clear();
+
+            //arrange
+            string name1 = "mohammed";
+            string name2 = "Ali";
+            List<string> list = new List<string>();
+            //Act
+            List<string> result = Program.AddContact(name1);  // {,mohammed}
+            result = Program.AddContact(name2);   // {mohammed,Ali}
+            result = Program.RemoveContact(name1);// {Ali}
+
+            bool name = result.Contains("mohammed");
+            //Assert
+            Assert.Equal(false, name);
+        }
+        [Fact]
+        public void ViewAllContacts()
+        {
+            Program.Contacts.Clear();
+
+            //Arrange
+            string name1 = "mohammed";
+            string name2 = "Ali";
+            //Act
+            List<string> result = Program.AddContact(name1);  // {,mohammed}
+            result = Program.AddContact(name2);    // {mohammed,Ali}
+            List<string> list = Program.Contacts;
+            result = Program.ViewAllContacts(list);          // {mohammed,Ali}
+            //Assert
+            Assert.Equal(2, result.Count);
+
 
         }
     }
+
 }
